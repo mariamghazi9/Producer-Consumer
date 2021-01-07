a9e8df2b17cc0c25804138202ff4d2ffb5293198
carnival <rasmaa99@yahoo.com>
2021-01-07 09:54:47 -0800

    myQueue

--- src/model/MyQueue.java
+++ src/model/MyQueue.java
@@ -5,31 +5,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 
-public class MyQueue implements Source,Consumer {
-    private class GraphicalQueue{
-        public Point getCoordinates() {
-            return coordinates;
-        }
-
-        public void setCoordinates(Point coordinates) {
-            this.coordinates = coordinates;
-        }
-
-        private Point coordinates;
-
+public class MyQueue implements Source, Consumer {
+    private GraphicalQueue graphicalQueue;
+    private List<Producer> readyMachines;
+    private ArrayBlockingQueue<Product> ProductsQueue;
+    public MyQueue() {
+        graphicalQueue = new GraphicalQueue();
+        readyMachines = new ArrayList<>();
+        ProductsQueue = new ArrayBlockingQueue<Product>(Manager.getInstance().getProductsNumber());
     }
 
     public GraphicalQueue getGraphicalQueue() {
         return graphicalQueue;
     }
-
-
-
-    private GraphicalQueue graphicalQueue=new GraphicalQueue();
-    private List<Producer> readyMachines=new ArrayList<>();
-    private ArrayBlockingQueue<Product> ProductsQueue=new ArrayBlockingQueue<Product>(1000);
-
-
 
     @Override
     public void update(Producer machine) {
@@ -38,9 +26,8 @@ public class MyQueue implements Source,Consumer {
 
     }
 
-    private synchronized void registerProduct()
-    {
-        Product product=ProductsQueue.remove();
+    private synchronized void registerProduct() {
+        Product product = ProductsQueue.remove();
         if (!readyMachines.isEmpty()) {
             Producer activeMachine = readyMachines.remove(0);
             activeMachine.serve(product);
@@ -51,7 +38,20 @@ public class MyQueue implements Source,Consumer {
     @Override
     public void consume(Product product) {
 
-      ProductsQueue.add(product);
+        ProductsQueue.add(product);
         registerProduct();
+    }
+
+    private class GraphicalQueue {
+        private Point coordinates;
+
+        public Point getCoordinates() {
+            return coordinates;
+        }
+
+        public void setCoordinates(Point coordinates) {
+            this.coordinates = coordinates;
+        }
+
     }
 }
