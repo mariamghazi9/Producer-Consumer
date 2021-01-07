347e5c9bc47cb607acd229f7ce20ba41649750e2
Mo'men Hamdy <53626982+Mo-hamdy2000@users.noreply.github.com>
2021-01-07 18:36:58 +0200

    GUI layout

--- src/controllers/Simulator.java
+++ src/controllers/Simulator.java
@@ -1,24 +1,78 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Simulator {
+
+public class Simulator implements Initializable {
 
     @FXML
     Canvas canvas;
+    @FXML
+    Button deleteButton;
+    @FXML
+    Button addQueue;
+    @FXML
+    Button addMachine;
+    @FXML
+    TextField productsNumText;
 
+    private boolean deleteFlag = false;
+    private boolean isQueue = true;
 
     @FXML
     public void clicked(MouseEvent event) {
+        deleteButton.getStyleClass().add("button-style-disable");
         if(event.getClickCount() == 2){
             System.out.println("Double clicked");
-            addMachine(canvas.getGraphicsContext2D(), (int)event.getX(), (int)event.getY());
+            if (isQueue) {
+                addQueue(canvas.getGraphicsContext2D(), (int) event.getX(), (int) event.getY());
+            } else {
+                addMachine(canvas.getGraphicsContext2D(), (int) event.getX(), (int) event.getY());
+            }
+        }
+    }
+
+    @FXML
+    public void deleteConnection() {
+        if (deleteFlag) {
+            deleteButton.getStyleClass().remove(1);
+        } else {
+            deleteButton.getStyleClass().add("disable");
+        }
+        deleteFlag = !deleteFlag;
+    }
+
+    @FXML
+    protected void handleAddQueue() {
+        toggleInsert(true);
+    }
+
+    @FXML
+    protected void handleAddMachine() {
+        toggleInsert(false);
+    }
+
+    public void toggleInsert(boolean isQueue) {
+        if (isQueue != this.isQueue) {
+            if (isQueue) {
+                addMachine.getStyleClass().remove(1);
+                addQueue.getStyleClass().add("selected");
+            } else {
+                addQueue.getStyleClass().remove(1);
+                addMachine.getStyleClass().add("selected");
+            }
+            this.isQueue = isQueue;
         }
     }
 
@@ -31,7 +85,7 @@ public class Simulator {
     }
 
     private void addMachine(GraphicsContext gc, int x, int y) {
-        gc.setFill(Color.GREEN);
+        gc.setFill(Color.RED);
         int radius = 25;
         gc.fillOval(x-radius, y-radius, 2*radius, 2*radius);
         gc.setFont(Font.font ("Verdana", 10));
@@ -39,5 +93,9 @@ public class Simulator {
         gc.fillText("M1", x-8, y);
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        addQueue.getStyleClass().add("selected");
+    }
 }
 
