78cedca84410d5222175aa01cecd6b832ae35877
Ahmad Waleed <a.waleedothman@gmail.com>
2021-01-07 19:59:35 +0200

    manager class altered

--- src/model/Manager.java
+++ src/model/Manager.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 
 public class Manager {
 
-    ArrayList<Product> products = new ArrayList<>();
+    private State currentState;
+    private final ArrayList<State> savedStates = new ArrayList<>();
     final private static Manager instance = new Manager();
     private int productsNumber;
 
@@ -21,5 +22,51 @@ public class Manager {
 
     public void setProductsNumber(int productsNumber) {
         this.productsNumber = productsNumber;
+    }
+
+    public MyQueue addQueue() {
+        if (currentState == null) currentState = new State();
+        MyQueue myQueue = new MyQueue();
+        currentState.getQueues().add(myQueue);
+        return myQueue;
+
+    }
+
+    public void removeQueue(MyQueue myQueue) {
+        currentState.getQueues().remove(myQueue);
+    }
+
+    public Machine addMachine() {
+        if (currentState == null) currentState = new State();
+        Machine machine = new Machine();
+        currentState.getMachines().add(machine);
+        return machine;
+    }
+
+    public void removeMachine(Machine machine) {
+        currentState.getMachines().remove(machine);
+    }
+
+    public void startSimulation() {
+        ArrayList<MyQueue> queues = currentState.getQueues();
+        ArrayList<Product> products = currentState.getProducts();
+        for (MyQueue queue: queues) {
+
+        }
+
+        for (int i = 0; i < this.productsNumber; i++) {
+            products.add(new Product());
+        }
+
+        this.saveState();
+    }
+
+    private void saveState() {
+        this.savedStates.add(currentState);
+        currentState = null;
+    }
+
+    public void play(int stateIndex) {
+
     }
 }
